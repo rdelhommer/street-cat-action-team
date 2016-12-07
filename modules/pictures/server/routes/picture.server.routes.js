17a8d5b19bb5e62885c77b303ee8a6d8ec77eec8
Rick Delhommer <rdelhommer@gmail.com>
2016-12-06 21:12:13 -0800

    remove comments

--- modules/pictures/server/routes/picture.server.routes.js
+++ modules/pictures/server/routes/picture.server.routes.js
@@ -5,17 +5,14 @@
   var controller = require('../controllers/picture.server.controller');
 
   module.exports = function (app) {
-    // Cats collection routes
     app.route('/api/pictures').all(policy.isAllowed)
       .get(controller.list)
       .post(controller.create);
 
-    // Single cat routes
     app.route('/api/pictures/:pictureId').all(policy.isAllowed)
       .get(controller.read)
       .delete(controller.delete);
 
-    // Finish by binding the cat middleware
     app.param('pictureId', controller.pictureById);
   };
 }());
